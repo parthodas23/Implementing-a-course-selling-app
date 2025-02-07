22ea690a0dc8cb59985cbf12a3c9dbc734717640
Partha Das <parthodasm@gmail.com>
2025-02-08 00:21:46 +0600

    Updated project structure and secured MongoDB connection

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://parthodasm23:armstrong153@cluster0.95xzc.mongodb.net/Express-Db"
-);
+
+// Use environment variable for MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const AdminSchema = new mongoose.Schema({
   username: String,
